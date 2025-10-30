340deb6ac7506021fd0c62530289e8bc4258a84d
FredBert66 <fred.bertenstam@avendit.se>
2025-10-30 11:07:33 +0100

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,11 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 // NOTE: This file should not be edited
-{
-  "compilerOptions": {
-    "baseUrl": "src",
-    "paths": { "@/*": ["*"] },
-    "resolveJsonModule": true,
-    "moduleResolution": "Bundler"
-  }
-}
